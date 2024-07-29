aeeccd32621a574bd85a598d113a518497869641
mnevison <mnevison2@gmail.com>
2024-07-30 00:45:38 +0100

    added: new file for game specific assets - started JS for game

---
+++ assets/js/game.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const friends = [
+    { src: "assets/images/game-images/dusty.png", alt: "dusty image" },
+    { src: "assets/images/game-images/indy.jpg", alt: "indy image" },
+    { src: "assets/images/game-images/judo.png", alt: "judo image" },
+    { src: "assets/images/game-images/snickers.png", alt: "snickers image" },
+    { src: "assets/images/game-images/socks.png", alt: "socks image" },
+  ];
+
+  const friendCards = document.querySelector(".friend-cards");
+
+  let cardFlipTemp = [];
+  
+});
