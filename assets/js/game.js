c62afeb858455772c8f4066739e3241427f23d0c
mnevison <mnevison2@gmail.com>
2024-07-30 01:07:54 +0100

    added: function to create cards

--- assets/js/game.js
+++ assets/js/game.js
@@ -10,5 +10,31 @@ document.addEventListener("DOMContentLoaded", function () {
   const friendCards = document.querySelector(".friend-cards");
 
   let cardFlipTemp = [];
-  
+
+  function createFriendCard() {
+    const cardContainer = document.createElement("div");
+    cardContainer.classList.add("card-container");
+
+    const faceCard = document.createElement("div");
+    faceCard.classList.add("face-card");
+
+    const showCard = document.createElement("div");
+    showCard.classList.add("show-card");
+
+    const frontCard = document.createElement("div");
+    frontCard.classList.add("front-card");
+
+    faceCard.appendChild(showCard);
+    faceCard.appendChild(frontCard);
+
+    cardContainer.appendChild(faceCard);
+
+    const cardHidden = document.createElement("img");
+    cardHidden.src = "assets/images/game-images/question-mark.jpeg";
+    cardHidden.alt = "question mark box"
+    showCard.appendChild(cardHidden)
+
+    return cardContainer
+
+  }
 });
