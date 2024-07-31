a19627e1c6fcdb55d7eee47f27c78ea9bcf5af1b
mnevison <mnevison2@gmail.com>
2024-07-31 14:53:10 +0100

    added: comments to audio.js

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,15 +1,20 @@
+/* jshint esversion: 6 */
+
+// Tracks if audio is currently playing
 let isPlaying = false;
+// Stores currently playing audio
 let currentAudio = null;
 
+// Select the specific audio element with the specified source
 function toggleAudio(audioSrc) {
   let audio = document.querySelector('.myAudio[src="' + audioSrc + '"]');
-
+// If there is already audio playing, pause and reset its time
   if (currentAudio && currentAudio !== audio) {
     currentAudio.pause();
     currentAudio.currentTime = 0;
     isPlaying = false;
   }
-
+  // If the selected audio is not currently playing, or if a different audio was playing, play it
   if (!isPlaying || currentAudio !== audio) {
     audio.play();
     isPlaying = true;
@@ -18,6 +23,7 @@ function toggleAudio(audioSrc) {
       .querySelector(".audio-button svg")
       .setAttribute("class", "bi bi-volume-off");
   } else {
+    // If audio is playing, pause it
     audio.pause();
     isPlaying = false;
     currentAudio = null;
